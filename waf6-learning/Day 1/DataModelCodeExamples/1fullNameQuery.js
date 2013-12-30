4ee52518abc4d70313de5fabb5836299ed0a868f
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-12-30 23:11:34 +0000

    Update #9

---
+++ waf6-learning/Day 1/DataModelCodeExamples/1fullNameQuery.js
@@ -0,0 +1,8 @@
+﻿var x = ds.Person.query('ID < 100');
+//x = ds.Person.query('fullname == "John Smith"');
+//x = ds.Person.query('fullname == "Burke"');
+//x = ds.Person.query('fullname == "Brand*"');
+//x = ds.Person.query('fullname >= "Bill Wimmer');
+//x = ds.Person.query('fullname == "V* B*"');
+//x = ds.Person.query('fullname >= "John Smith" order by fullname');
+x.toArray('firstName, lastName, fullName'); // returns results
