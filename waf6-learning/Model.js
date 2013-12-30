9e4013d7395a7bbc3ac350ab3ceb7209421a4354
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-12-30 23:11:17 +0000

    Update #7

---
+++ waf6-learning/Model.js
@@ -0,0 +1,30 @@
+﻿
+guidedModel =// @startlock
+{
+	Person :
+	{
+		address :
+		{
+			onGet:function()
+			{// @endlock
+				// Add your code here
+				return this.streetNumber + this.streetName;
+			}// @startlock
+		},
+		fullName :
+		{
+			onSet:function(value)
+			{// @endlock
+				// Add your code here
+				var names = value.split(' ');
+				this.firstName = names[0];
+				this.lastName = names[1];
+			},// @startlock
+			onGet:function()
+			{// @endlock
+				// Add your code here
+				return this.firstName + " " + this.lastName;
+			}// @startlock
+		}
+	}
+};// @endlock
